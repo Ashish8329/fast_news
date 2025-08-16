0956d617d22c24f04c9e71b3310d22f088b83197
Ashish Auti <ashishauti123@gmail.com>
2025-08-16 15:23:43 +0530

    feat : add user models

--- backend/fast_news/user/admin.py
+++ backend/fast_news/user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import User
+
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "password",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = ["created_at", "updated_at"]
--- backend/fast_news/user/models.py
+++ backend/fast_news/user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField(
+        max_length=100,
+        help_text="user_name",
+    )
+    email = models.EmailField(
+        unique=True,
+        help_text="user_email",
+    )
+    first_name = models.CharField(
+        max_length=100, blank=True, null=True, help_text="first_name"
+    )
+    last_name = models.CharField(
+        max_length=100, help_text="last_name", blank=True, null=True
+    )
+    password = models.CharField(max_length=100, help_text="password")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # class Meta:
+    #     ordering = ("-created_at",)
